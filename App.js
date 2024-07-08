e72698353b4b66a2b9c97f0787049fedeed34d09
Sravanthi-Proddatoori <118324864+Sravanthi-Proddatoori@users.noreply.github.com>
2024-07-08 18:26:28 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LogiinSignup from './Components/LoginSignup/LogiinSignup';
+
+function App() {
+  return (
+    <div>
+      <LogiinSignup/>
+
+    </div>
+  );
+}
+
+export default App;
